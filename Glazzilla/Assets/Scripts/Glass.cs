884a1d4ee0c5e079b216c43ccfedd46fe7f0472b
fzlink <fazlibozatay@gmail.com>
2019-09-30 21:06:33 +0300

    Physics made possible

--- Glazzilla/Assets/Scripts/Glass.cs
+++ Glazzilla/Assets/Scripts/Glass.cs
@@ -16,7 +16,7 @@ public class Glass : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //rigidbody.velocity = new Vector2(3f, 2f);
+        //rigidbody.velocity = new Vector2(15f, 8f);
         
     }
 
@@ -26,17 +26,19 @@ public class Glass : MonoBehaviour
         
         if (Input.GetMouseButtonDown(0))
         {
-
-
-
-
-            
-
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log("MousePos = " + mousePos);
 
             var dir = (transform.position - mousePos).normalized;
-            float magnitude = 50f;
+
+            /////////
+            var angle = Mathf.Atan2(dir.y,dir.x);
+            Debug.Log(angle);
+            Debug.Log(Mathf.Rad2Deg * angle);
+            var angleInDeg = Mathf.Rad2Deg * angle;
+            //////////
+
+            float magnitude = 60f;
             RaycastHit2D raycast = Physics2D.Raycast(mousePos,dir);
             var collisionPoint = raycast.point;
             RaycastHit2D hit;
@@ -44,18 +46,40 @@ public class Glass : MonoBehaviour
             LayerMask layerMask;
             //Ray ray = Physics2D.Raycast(mousePos,dir,out hit,Mathf.Infinity,layerMask);
             
-            rigidbody.AddForceAtPosition(dir * magnitude, collisionPoint,ForceMode2D.Force);
+            rigidbody.AddForceAtPosition(new Vector3(dir.x, 1,0)*magnitude, collisionPoint,ForceMode2D.Force);
+
             float clampX = rigidbody.velocity.x;
             float clampY = rigidbody.velocity.y;
             clampX = Mathf.Clamp(clampX, 0f, 0.1f);
             clampY = Mathf.Clamp(clampY, 0f, 0.1f);
             rigidbody.velocity = new Vector2(clampX, clampY);
-
+            //rigidbody.AddTorque(Mathf.Log10(0.01f * angleInDeg )*10);
+            
         }
 
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 30;
+        }
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Floor") || collision.collider.CompareTag("Bug"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 20;
+            gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.eulerAngles = new Vector3(0, 0, 180);
+    }
+
+
 
 
 }
