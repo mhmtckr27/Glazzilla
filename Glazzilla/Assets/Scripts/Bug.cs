f1b9826ea9e7f2f1f176a379e47c5cfad7b0438f
fzlink <fazlibozatay@gmail.com>
2019-10-02 21:01:46 +0300

    Made changes

---
+++ Glazzilla/Assets/Scripts/Bug.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : MonoBehaviour
+{
+    [SerializeField] private Vector2[] wayPoints;
+    [SerializeField] private float bugSpeed;
+    [SerializeField] private int direction;
+    private bool inGlass = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeRotation();
+    }
+
+    private void changeRotation()
+    {
+        if (direction == -1)
+            gameObject.transform.eulerAngles = new Vector3(0, 180f, 0);
+        else
+            gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inGlass)
+            move();
+        else if(inGlass)
+            panic();
+    }
+
+    private void move()
+    {
+        if (direction == -1)
+            transform.position += new Vector3(wayPoints[0].x * Time.deltaTime, 0, 0);
+        else if (direction == 1)
+            transform.position += new Vector3(wayPoints[1].x * Time.deltaTime, 0, 0);
+        changeDirection();
+    }
+    private void changeDirection()
+    {
+        if (direction == -1 && transform.position.x < wayPoints[0].x)
+            direction = 1;
+        else if (direction == 1 && transform.position.x > wayPoints[1].x)
+            direction = -1;
+        changeRotation();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            inGlass = true;
+            direction = -direction;
+            changeRotation();
+            
+        }
+
+    }
+
+    private void panic()
+    {
+        transform.position +=new Vector3( bugSpeed * Time.deltaTime * direction,0,0);
+    }
+}
--- Glazzilla/Assets/Scripts/Glass.cs
+++ Glazzilla/Assets/Scripts/Glass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,60 +9,46 @@ public class Glass : MonoBehaviour
     [SerializeField] private float InitForce = 10;
     [SerializeField] private Rigidbody2D rigidbody;
     [SerializeField] private Camera camera;
-
-    private int screenWidthPerUnits = 9;
-    private int screenHeightPerUnits = 16;
-
+    [SerializeField] private GameState gameState;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rigidbody.velocity = new Vector2(15f, 8f);
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if (Input.GetMouseButtonDown(0))
-        {
+            bounce();
+        if (Input.GetMouseButtonDown(1))
+            strike();
+    }
+
+    private void strike()
+    {
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 30;
+    }
+
+    private void bounce()
+    {
+
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("MousePos = " + mousePos);
-
             var dir = (transform.position - mousePos).normalized;
-
-            /////////
-            var angle = Mathf.Atan2(dir.y,dir.x);
-            Debug.Log(angle);
-            Debug.Log(Mathf.Rad2Deg * angle);
-            var angleInDeg = Mathf.Rad2Deg * angle;
-            //////////
-
-            float magnitude = 60f;
-            RaycastHit2D raycast = Physics2D.Raycast(mousePos,dir);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            float magnitude = 30f;
+            RaycastHit2D raycast = Physics2D.Raycast(mousePos, dir);
             var collisionPoint = raycast.point;
-            RaycastHit2D hit;
-            Debug.DrawRay(mousePos, transform.position - mousePos, Color.green,15f);
-            LayerMask layerMask;
-            //Ray ray = Physics2D.Raycast(mousePos,dir,out hit,Mathf.Infinity,layerMask);
-            
-            rigidbody.AddForceAtPosition(new Vector3(dir.x, 1,0)*magnitude, collisionPoint,ForceMode2D.Force);
+            Debug.DrawRay(mousePos, transform.position - mousePos, Color.green, 15f);
+            rigidbody.AddForceAtPosition(new Vector3(dir.x * Mathf.PI/2 , 1, 0) * magnitude, collisionPoint, ForceMode2D.Force);
 
             float clampX = rigidbody.velocity.x;
             float clampY = rigidbody.velocity.y;
             clampX = Mathf.Clamp(clampX, 0f, 0.1f);
             clampY = Mathf.Clamp(clampY, 0f, 0.1f);
             rigidbody.velocity = new Vector2(clampX, clampY);
-            //rigidbody.AddTorque(Mathf.Log10(0.01f * angleInDeg )*10);
-            
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 30;
-        }
 
     }
 
@@ -71,12 +58,15 @@ public class Glass : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody2D>().gravityScale = 20;
             gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+            GetComponent<Glass>().enabled = false;
+
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.transform.eulerAngles = new Vector3(0, 0, 180);
+        
+        gameState.win();
     }
 
 }
